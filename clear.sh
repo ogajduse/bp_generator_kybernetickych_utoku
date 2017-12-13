047e579e12bec589b71e4d0bcc28ee125c1f940f
Ondřej Gajdušek <ogajduse@redhat.com>
2017-12-13 12:25:20 +0100

    přidán skript na čištění odkládacích souborů

---
+++ clear.sh
@@ -0,0 +1,5 @@
+rm sablona-prace.lof  sablona-prace.log  sablona-prace.lol  \
+	sablona-prace.lot  sablona-prace.out  \
+	sablona-prace.toa  sablona-prace.toc
+rm ./*.aux ./*.log
+rm text/*.aux text/*.log
